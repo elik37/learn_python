f2b80b9f4ca8ff435dece33b98b690943047089b
elik37 <k8295081@gmail.com>
2024-04-07 12:55:33 +0300

    Fix

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-# text: str = "Hello Elik"
-#
-# def count_vowels(string: str) -> None:
-#     VOWELS: str = 'aeiouAEIOU'
-#     count: int = 0
-#
-#     for letter in string:
-#         if letter in VOWELS:
-#             count = count + 1
-#
-#     print(count)
-#
-# count_vowels(string=text)
-#
-# def print_player_name(name: str):
-#     text = f'Futboler: {name}'
-#     print(text)
-#
-# player_1 = 'Ronaldo'
-# print_player_name(name=player_1)
-#
-# player_2 = 'Messi'
-# print_player_name(name=player_2)
-#
-# player_4 = 'Ibrahimovic'
-# print_player_name(name=player_4)
-#
-# player_5 = 'Shevchenko'
-# print_player_name(name=player_5)
+text: str = "Hello Elik"
+
+def count_vowels(string: str) -> None:
+    VOWELS: str = 'aeiouAEIOU'
+    count: int = 0
+
+    for letter in string:
+        if letter in VOWELS:
+            count = count + 1
+
+    print(count)
+
+count_vowels(string=text)
+
+def print_player_name(name: str):
+    text = f'Futboler: {name}'
+    print(text)
+
+player_1 = 'Ronaldo'
+print_player_name(name=player_1)
+
+player_2 = 'Messi'
+print_player_name(name=player_2)
+
+player_4 = 'Ibrahimovic'
+print_player_name(name=player_4)
+
+player_5 = 'Shevchenko'
+print_player_name(name=player_5)
