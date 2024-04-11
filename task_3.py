13739156072def06f10651039a2a9ee4dfb64ff3
elik37 <k8295081@gmail.com>
2024-04-11 19:57:18 +0300

    New task 3

---
+++ task_3.py
@@ -0,0 +1,16 @@
+def main() -> None:
+    nums: list = list(range(1, 11))
+    REQUIRED_NUMBER: int = 7
+
+    def num_collection(nums: list) -> None:
+        for num in nums:
+            if num == REQUIRED_NUMBER:
+                print('Число 7 знайдене')
+            else:
+                print('Число не вірне')
+
+    num_collection(nums)
+
+
+if __name__ == '__main__':
+    main()
